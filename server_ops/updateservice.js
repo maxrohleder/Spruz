4f43a4a5c6ef8caf7f3d57d0242a3c832aafd3b9
Maximilian Rohleder <Maxi.Rohleder@fau.de>
2021-12-14 22:47:11 +0100

    update backend

--- server_ops/updateservice.js
+++ server_ops/updateservice.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const shell = require('shelljs');
 
+// ust express app with json middleware
 const app = express();
+app.use(express.json());
+
+
 const port = 3000;
 
 // sent from github to validate the request
-const secret = 'asklbpwqiu3r[23oc32';
+const secret = '<insert-on-server>';
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.post('/update', (req, res) => {
-
+  console.log(req.body);
+  if (req.body.user == 'mr' && req.body == secret){
     shell.exec('./restart_docker_container.sh')
     console.log("restarted docker container.")
-
-    // send response
-    res.status(200).send('OK');
-    console.log("sent response!")
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(503);
+  }
 })
 
 app.listen(port, () => {
